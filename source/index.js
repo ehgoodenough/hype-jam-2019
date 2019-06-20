c53d58d31cb0cbf338e17e099ca0856be97a7dd4
Andrew McPherson <mcphandr@amazon.com>
2019-06-20 16:58:02 -0700

    Added a frame and more emoji and a title.

--- source/index.js
+++ source/index.js
@@ -1,6 +1,9 @@
 import Preact from "preact"
 import Yaafloop from "yaafloop"
 
+import "views/Emoji.view.less"
+import "views/Frame.view.less"
+
 require("statgrab/do")
 
 let loop = new Yaafloop(function(delta) {
@@ -9,59 +12,55 @@ let loop = new Yaafloop(function(delta) {
 
 let images = require.context("images/blobs", true)
 images = images.keys().map((key) => images(key))
+images.push(images[2])
+images.push(images[4])
+images.push(images[8])
 
-const emoji = [
-    {"x": 0, "y": 0, "image": images[0]},
-    {"x": 1, "y": 0, "image": images[1]},
-    {"x": 2, "y": 0, "image": images[2]},
-    {"x": 3, "y": 0, "image": images[3]},
-    {"x": 4, "y": 0, "image": images[4]},
-    {"x": 5, "y": 0, "image": images[5]},
-    // ...
-    {"x": 0, "y": 1, "image": images[6]},
-    {"x": 1, "y": 1, "image": images[7]},
-    {"x": 2, "y": 1, "image": images[8]},
-    {"x": 3, "y": 1, "image": images[9]},
-    {"x": 4, "y": 1, "image": images[10]},
-    {"x": 5, "y": 1, "image": images[11]},
-    // ...
-    {"x": 0, "y": 2, "image": images[12]},
-    {"x": 1, "y": 2, "image": images[13]},
-    {"x": 2, "y": 2, "image": images[14]},
-    {"x": 3, "y": 2, "image": images[15]},
-    {"x": 4, "y": 2, "image": images[16]},
-    {"x": 5, "y": 2, "image": images[17]},
-]
-
-console.log(images.length)
+const JITTER = 0.15
 
 class View {
     render() {
         return (
             <div class="View">
-                {emoji.map((emoji) => (
-                    <Emoji emoji={emoji}/>
-                ))}
+                <title>Hype Jam</title>
+                <Frame>
+                    <div class="Title">
+                        Hype Jam
+                    </div>
+                    <div class="Emojis">
+                        {images.map((image) => (
+                            <Emoji image={image}/>
+                        ))}
+                    </div>
+                </Frame>
             </div>
         )
     }
 }
 
-import "views/Emoji.view.less"
-
-const JITTER = 0.1
+class Frame {
+    render() {
+        return (
+            <div class="Frame" id="frame">
+                {this.props.children}
+            </div>
+        )
+    }
+}
 
 class Emoji {
     render() {
         return (
-            <div class="Emoji" style={this.style}/>
+            <div class="Emoji">
+                <div class="Image" style={this.style}/>
+            </div>
         )
     }
     get style() {
         return {
-            "top": this.props.emoji.y + "em",
-            "left": this.props.emoji.x + "em",
-            "background-image": "url(" + this.props.emoji.image + ")",
+            // "top": this.props.emoji.y + "em",
+            // "left": this.props.emoji.x + "em",
+            "background-image": "url(" + this.props.image + ")",
             "margin-top": (Math.random() * JITTER) - (JITTER / 2) + "em",
             "margin-left": (Math.random() * JITTER) - (JITTER / 2) + "em",
         }
